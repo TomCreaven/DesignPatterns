8822b55e638c266445f0fb0e55b327691442c01c
Tom Creaven <tom.creaven@tesco.com>
2019-12-27 15:26:04 +0000

    Made singleton fully thread safe.

--- src/com/pluralsight/singleton/DbSingleton.java
+++ src/com/pluralsight/singleton/DbSingleton.java
@@ -1,16 +1,25 @@
 package com.pluralsight.singleton;
 
 public class DbSingleton {
+    //ToDo check volatile and also reflection class meaning
 
-    private static DbSingleton instance = null;
+    private static volatile DbSingleton instance = null;
 
-    private DbSingleton() {}
+    private DbSingleton() {
+        if (instance !=null) {
+            throw new RuntimeException("Use getInstance() method to create");
+        }
+    }
 
+    // if we make whole nethod synchronised this would be a performance hit,
     public static DbSingleton getInstance(){
-        //making this lazily loaded can be a good impact on performance.
-        // This is not thread safe
-        if (instance == null){
-            instance = new DbSingleton();
+        if (instance == null) {
+            synchronized (DbSingleton.class) {
+                //once we've synchronized we need to do a null check once more since two instances may have been trying to enter this code block before they were synchronized.
+                if (instance == null) {
+                    instance = new DbSingleton();
+                }
+            }
         }
         return  instance;
     }
