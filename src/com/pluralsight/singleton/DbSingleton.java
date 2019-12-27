20fef6e17ba74ed623f8567c96e120a5676fbfe9
Tom Creaven <tom.creaven@tesco.com>
2019-12-27 15:15:14 +0000

    Changed singleton to be lazily loaded. Added null check to create instance if one does not exist. This enhances performance as there is no overhead at startup to create instsances at startup. This is not threadsafe however

--- src/com/pluralsight/singleton/DbSingleton.java
+++ src/com/pluralsight/singleton/DbSingleton.java
@@ -2,11 +2,16 @@ package com.pluralsight.singleton;
 
 public class DbSingleton {
 
-    private static DbSingleton instance = new DbSingleton();
+    private static DbSingleton instance = null;
 
     private DbSingleton() {}
 
     public static DbSingleton getInstance(){
+        //making this lazily loaded can be a good impact on performance.
+        // This is not thread safe
+        if (instance == null){
+            instance = new DbSingleton();
+        }
         return  instance;
     }
 
